aa8ad93a9586126e18afde0c8eca9ebd8d4db15e
David Alpert <david@spinthemoose.com>
2011-03-02 19:56:50 -0600

    upgraded Machine.UrlStrong.Sample to MVC3/.NET4, urlstrong prebuildstep is working as expected

--- Source/Machine.UrlStrong.Sample/Controllers/HomeController.cs
+++ Source/Machine.UrlStrong.Sample/Controllers/HomeController.cs
@@ -2,14 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Web.Mvc;
 
 namespace Machine.UrlStrong.Sample.Controllers
 {
-  public class HomeController
+  public class HomeController : Controller
   {
     public void Index()
     {
-      Redirect(Urls.root.user[3].friend.list);
+      //Redirect(Urls.root.user[3].friend.list);
+        Redirect(Urls.root.some.strange.action);
     }
 
     public void Redirect(ISupportGet url)
